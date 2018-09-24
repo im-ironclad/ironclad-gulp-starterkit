73707fd398ca3316dc185e8c6afa3db3e8cd1594
Adam Wells <awells@wrayward.com>
2018-09-23 20:05:54 -0400

    Changing from "playground" terminology

--- public/index.php
+++ public/index.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Playground</title>
+  <title>Ironclad Starter Kits</title>
 
   <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
   <h1>
-    Playground
+    Gulp SCSS &amp; Vanilla JS Starter Kit
   </h1>
 
   <script src="scripts/index.js"></script>
